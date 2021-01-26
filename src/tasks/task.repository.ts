eb018fa494488c5cdfc9838c74e358b00abb1662
kiore <ricardochicas95@gmail.com>
2021-01-25 18:04:11 -0600

    remove uuid import

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,7 +1,6 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { Task } from './task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { v4 as uuidv4 } from 'uuid';
 import { TaskStatus } from './task-status.enum';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 
@@ -24,7 +23,6 @@ export class TaskRepository extends Repository<Task> {
   async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
     const { title, description } = createTaskDto;
     const task = new Task();
-    task.id = uuidv4();
     task.title = title;
     task.description = description;
     task.status = TaskStatus.OPEN;
